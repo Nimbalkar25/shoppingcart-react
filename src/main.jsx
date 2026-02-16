3a075e994faf5e4c97d88ca01c428df268764830
Pratik Nimbalkar <nimbalkarpratik25@gmail.com>
2026-02-16 19:24:18 +0530

    Browser Router change to hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from './redux/store.js'
 import {Provider} from "react-redux"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/shoppingcart-react">
+  <HashRouter basename="/shoppingcart-react">
     <Provider store ={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 
 )
