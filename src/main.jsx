36d09084f600bf81e373621a48ef30c35e3addc1
Pratik Nimbalkar <nimbalkarpratik25@gmail.com>
2026-02-16 19:26:03 +0530

    remove bas from hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from './redux/store.js'
 import {Provider} from "react-redux"
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/shoppingcart-react">
+  <HashRouter >
     <Provider store ={store}>
       <App />
     </Provider>
